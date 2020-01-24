eecc2a7687b9b6ab95e1eb74f201c3a6fe4102ba
justinelai <justine.lai@gmail.com>
2020-01-24 12:02:45 -0500

    still not pathfinding correctly

--- path.py
+++ path.py
@@ -15,7 +15,7 @@ class Path:
         current = starting_room
         stack.append([(current, "")])
 
-        # IN PROGRESS: Keep track of directions
+        # IN PROGRESS: Need to keep track of directions for when it backtracks?
         reverse_direction = {'n': 's', 's': 'n', 'w': 'e', 'e': 'w'}
 
         while len(stack) > 0: 
@@ -30,18 +30,12 @@ class Path:
             print(vertex)
             if vertex.id not in self.graph:
                 self.graph[vertex.id] = {} # Establish dict entry for this room.
-
                 for next_dir in vertex.get_exits(): # Get all exits
-
                     self.graph[vertex.id][next_dir] = '?' # Initialize key(s) in graph for this exit
-
                     next_vert = vertex.get_room_in_direction(next_dir) # Get exit room number
-                    
                     if next_vert not in self.graph: # If room hasn't been explored
                         self.graph[vertex.id][next_dir] = next_vert.id # Set exit room ID in graph
-
                         new_path = list(advpath) # Make a copy of path rather than reference
-                        
                         new_path.append((next_vert, next_dir)) # Append this new vert and provide what direction it was
                         move_log.append(next_dir)
                         stack.append(new_path) # Add new room to stack
